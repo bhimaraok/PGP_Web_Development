f37db7bfaaa0788014103a713efe05a8a905269b
Bhimarao Kulkarni <69769799+bhimaraok@users.noreply.github.com>
2022-01-16 02:14:42 +0530

    Add files via upload

---
+++ project_calender-2022/js/main.js
@@ -0,0 +1,36 @@
+function jan22() {
+    document.getElementById('slider').style.marginLeft='0%';
+}
+function feb22() {
+    document.getElementById('slider').style.marginLeft='-100%';
+}
+function mar22() {
+    document.getElementById('slider').style.marginLeft='-200%';
+}
+function apr22() {
+    document.getElementById('slider').style.marginLeft='-300%';
+}
+function may22() {
+    document.getElementById('slider').style.marginLeft='-400%';
+}
+function jun22() {
+    document.getElementById('slider').style.marginLeft='-500%';
+}
+function jul22() {
+    document.getElementById('slider').style.marginLeft='-600%';
+}
+function aug22() {
+    document.getElementById('slider').style.marginLeft='-700%';
+}
+function sep22() {
+    document.getElementById('slider').style.marginLeft='-800%';
+}
+function oct22() {
+    document.getElementById('slider').style.marginLeft='-900%';
+}
+function nov22() {
+    document.getElementById('slider').style.marginLeft='-1000%';
+}
+function dec22() {
+    document.getElementById('slider').style.marginLeft='-1100%';
+}
