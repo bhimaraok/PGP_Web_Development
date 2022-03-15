3af7f90435445fdebe100a04de9b172e0e707b29
Bhimarao Kulkarni <69769799+bhimaraok@users.noreply.github.com>
2022-03-16 02:13:18 +0530

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+    if (window.pageYOffset >= 102) {
+        document.getElementById('header').style.backgroundColor = 'rgb(13, 170, 144)';
+        document.getElementById('logo').style.color = 'white';
+
+    } else {
+        document.getElementById('header').style.backgroundColor = 'rgba(33, 153, 133, 1)';
+        document.getElementById;
+        document.getElementById('logo').style.color = 'rgb(255, 4, 4)';
+
+    }
+}
