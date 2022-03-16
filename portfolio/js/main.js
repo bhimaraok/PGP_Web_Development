d9a7c9ea4d7cff13b989af81a91621ec20e03e65
Bhimarao Kulkarni <69769799+bhimaraok@users.noreply.github.com>
2022-03-16 09:42:45 +0530

    Update main.js

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,10 +1,10 @@
 window.onscroll = function () {
     if (window.pageYOffset >= 102) {
-        document.getElementById('header').style.backgroundColor = 'rgb(13, 170, 144)';
+        document.getElementById('header').style.backgroundColor = 'rgba(33, 153, 133, 5)';
         document.getElementById('logo').style.color = 'white';
 
     } else {
-        document.getElementById('header').style.backgroundColor = 'rgba(33, 153, 133, 1)';
+        document.getElementById('header').style.backgroundColor = 'rgb(13, 170, 144)';
         document.getElementById;
         document.getElementById('logo').style.color = 'rgb(255, 4, 4)';
 
